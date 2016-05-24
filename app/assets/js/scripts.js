adc82d3c98bee190668f4fdf5eecd6d0b779617a
Ajain Vivek <ajainvivek07@gmail.com>
2016-05-24 10:22:04 +0530

    Updated Packages

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,17 +1,12 @@
 /*!
- * fastshell
- * Fiercely quick and opinionated front-ends
- * https://HosseinKarami.github.io/fastshell
- * @author Hossein Karami
- * @version 1.0.5
- * Copyright 2016. MIT licensed.
+ * P2PDrop
  */
 (function ($, window, document, undefined) {
 
   'use strict';
 
   $(function () {
-    var url = "/assets/packages/";
+    var url = "https://www.dropbox.com/";
     var OSName="Unknown OS";
     if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
     else if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
@@ -19,20 +14,20 @@
     else if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
     if (OSName === "Windows" || OSName === "Unknown OS") {
-      $(".js-download-button").attr("href", url + "p2pdrop-win32-x64.zip");
+      $(".js-download-button").attr("href", url + "s/jcdmn2guv4r8h6g/P2PDrop-win32-x64.zip?dl=0");
       $(".js-download-button.btn-2").css("display", "block");
-      $(".js-download-button.btn-2").attr("href", url + "p2pdrop-win32-ia32.zip");
+      $(".js-download-button.btn-2").attr("href", url + "s/ku6bygm01tgfvv0/P2PDrop-win32-ia32.zip?dl=0");
       $(".support").text("For Window 7 or later.");
       $(".os").text(OSName + " (64bit)");
       $(".os2").text(OSName + " (32bit)");
     } else if (OSName === "MacOS") {
-      $(".js-download-button").attr("href", url + "p2pdrop-darwin-x64.zip");
+      $(".js-download-button").attr("href", url + "s/e2bytxioyxwfaf8/P2PDrop-darwin-x64.zip?dl=0");
       $(".support").text("For OSX 10.8 or later.");
       $(".os").text(OSName);
     } else {
-      $(".js-download-button").attr("href", url + "p2pdrop-linux-x64.zip");
+      $(".js-download-button").attr("href", url + "s/16rv2z03d7e977z/P2PDrop-linux-x64.zip?dl=0");
       $(".js-download-button.btn-2").css("display", "block");
-      $(".js-download-button.btn-2").attr("href", url + "p2pdrop-linux-ia32.zip");
+      $(".js-download-button.btn-2").attr("href", url + "s/18htpnggth8c2c8/P2PDrop-linux-ia32.zip?dl=0");
       $(".os").text(OSName + " (64bit)");
       $(".os2").text(OSName + " (32bit)");
     }
